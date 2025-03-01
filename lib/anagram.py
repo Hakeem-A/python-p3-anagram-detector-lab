df17c7c0832d14bf6e687a20a08335e601fd00d3
Abdi Mohamed <bashanesix@gmail.com>
2025-03-01 15:32:58 +0300

    passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = sorted(word.lower())
+
+    def match(self, possible_anagrams):
+        sorted_possible_anagrams = [sorted(possible_anagram.lower()) for possible_anagram in possible_anagrams]
+
+        return [word for word in possible_anagrams if sorted(word.lower()) == sorted(self.word)]
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
